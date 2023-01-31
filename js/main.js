cedcdf764f52c9862636c495f1c293d5029f3634
Maria <mariapiskunova51@gmail.com>
2023-01-31 16:16:02 +0300

    -

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$('.slider__wrapper').slick({
+ infinite: true,
+ slidesToShow: 2,
+ slidesToScroll: 2,
+ arrows: true,
+ appendArrows: $('.heading__arrows'),
+ prevArrow: $('.heading__arrows-prev'),
+ nextArrow: $('.heading__arrows-next'),
+ waitForAnimate: false,
+ }
+)
